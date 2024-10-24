08b30bb8b01e5686b149a50254f7092ec14f8655
노예찬 <71473074+noy3928@users.noreply.github.com>
2024-10-25 07:23:38 +0900

    feat : make simple sse example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Controller()
+interface MessageEvent {
+  data: string | object;
+}
+
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Sse('event')
+  sendEvent(): Observable<MessageEvent> {
+    return interval(1000).pipe(map((num) => ({ data: `Event number ${num}` })));
   }
 }
